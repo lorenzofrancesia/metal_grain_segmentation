998428361479e1a44546c6b1e19cd8595f8610e2
lorenzofrancesia <lorenzo.francesia@gmail.com>
2025-01-20 11:12:48 +0100

    Update dataset.py for debugging

--- data/dataset.py
+++ data/dataset.py
@@ -2,7 +2,6 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2
 
 import torch
 from torch.utils.data import Dataset
@@ -10,13 +9,22 @@ from torchvision import transforms
 
 class SegmentationDataset(Dataset):
     
-    def __init__(self, image_dir, mask_dir, transform=None):
+    def __init__(self, image_dir, mask_dir, transform=transforms.ToTensor, normalize=False):
         
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.image_paths = sorted(os.listdir(image_dir))
         self.mask_paths = sorted(os.listdir(mask_dir))
         self.transform = transform
+        self.mean = None
+        self.std = None
+        self.normalize = normalize
+        
+        self._validate_dataset()
+        self._dataset_statistics()
+        
+        if self.normalize:
+            self.calculate_normalization()
     
     def __len__(self):
         return len(self.image_paths)
@@ -28,17 +36,67 @@ class SegmentationDataset(Dataset):
         image = Image.open(img_path).convert("RGB")
         mask = Image.open(mask_path).convert("L")
         
+        if self.normalize and self.mean is not None and self.std is not None:
+            image = self._normalize_image(image)
+        
         if self.transform:
             image, mask = self.transform(image, mask)
             
         return image, mask
     
+    def _validate_dataset(self):
+        if len(self.image_paths) != len(self.mask_paths):
+            raise ValueError("The number of images and masks do not match.")
+
+    
+    def _dataset_statistics(self):
+        image_sizes = []
+        for img_path in self.image_paths:
+            img = Image.open(os.path.join(self.image_dir, img_path))
+            image_sizes.append(img.size)
+        
+        widths, heights = zip(*image_sizes)
+        print(f"Number of images: {len(self.image_paths)}")
+        print(f"Min width: {min(widths)}, Max width: {max(widths)}")
+        print(f"Min height: {min(heights)}, Max height: {max(heights)}")
+        
+    def calculate_normalization(self):
+        pixel_sum = None
+        pixel_squared_sum = None
+        total_pixels = 0
+        
+        for idx in range(len(self)):
+            image, _ = self[idx]
+            
+            if isinstance(image, torch.Tensor):
+                image = image.numpy()
+            
+            if pixel_sum is None:
+                pixel_sum = np.zeros(image.shape[0])
+                pixel_squared_sum = np.zeros_like(pixel_sum)
+                
+            pixel_sum += image.sum(axis=(1,2))
+            pixel_squared_sum += (image ** 2).sum(axis=(1,2))
+            total_pixels += image.shape[1]*image.shape[2]
+            
+        self.mean = pixel_sum / total_pixels
+        self.std = np.sqrt(pixel_squared_sum / total_pixels - self.mean**2)
+        
+    def _normalize_image(self, image):
+
+        image_tensor = torch.tensor(image.getdata()).view(3, *image.size[::-1]) / 255.0
+        
+        normalize_transform = transforms.Normalize(mean=self.mean.tolist(), std=self.std.tolist())
+        normalized_tensor = normalize_transform(image_tensor)
+        
+        normalized_image = Image.fromarray(normalized_tensor.permute(1,2,0).numpy() * 255).astype('uint8')
+        return normalized_image
+
     
 class SegmentationTransform:
     def __init__(self, resize=(128,128)):
         self.resize = transforms.Resize(resize)
         self.to_tensor = transforms.ToTensor()
-        self.normalize = transforms.Normalize(mean=[0.5], std=[0.5])
         
     def __call__(self, image, mask):
         image = self.resize(image)
@@ -112,6 +170,7 @@ def class_distribution(dataset, classes_names=['Background', 'Foreground']):
                  ha="center", va="bottom", fontsize=10, color="black")
     plt.show()
 
+
 def visualize_overlay(dataset, idx=None, alpha=0.5):
     
     if idx is None:
@@ -141,8 +200,27 @@ if __name__ == '__main__':
     image_dir = os.path.join(path, "images")
     mask_dir = os.path.join(path, "masks")
     
-    transform = SegmentationTransform()
-    dataset = SegmentationDataset(image_dir=image_dir, mask_dir=mask_dir, transform=transform)
+    dataset_norm = SegmentationDataset(image_dir=image_dir, mask_dir=mask_dir, normalize=True)
+    # dataset = SegmentationDataset(image_dir=image_dir, mask_dir=mask_dir, transform=transform)
     
+    # img, _ = dataset[0]
+    img_norm, _ = dataset_norm[0]
+    
+    # img = image_for_plot(img)
+    img_norm = image_for_plot(img_norm)
+    
+    # # Plot the images side by side
+    # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    # axes[0].imshow(img)
+    # axes[0].set_title("Original Image")
+    # axes[0].axis("off")
+    # axes[1].imshow(img_norm)
+    # axes[1].set_title("Normalized Image")
+    # axes[1].axis("off")
+
+    # plt.tight_layout()
+    # plt.show()
+
     #class_distribution(dataset)
-    visualize_overlay(dataset)
+    #visualize_overlay(dataset)
+    
